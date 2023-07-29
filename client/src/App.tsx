c66b577e647331d072703f375a95f92d6e69815f
Tae Hyoung Jo <taehyoung.jo@gmail.com>
2023-07-29 16:44:56 -0700

    feed with posts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
+import { BiComment } from "react-icons/bi";
 import "./App.css";
 
-type Feed = {
+type Post = {
   id: string;
   username: string;
   content: string;
@@ -11,7 +12,7 @@ type Feed = {
   timestamp: number;
 };
 
-const DUMMY_FEED: Feed[] = [
+const DUMMY_FEED: Post[] = [
   {
     id: "1",
     username: "johndoe",
@@ -21,21 +22,45 @@ const DUMMY_FEED: Feed[] = [
     comments: 0,
     timestamp: Date.now(),
   },
+  {
+    id: "2",
+    username: "kevinhu",
+    content: "Hello, world!",
+    likes: 0,
+    retweets: 0,
+    comments: 0,
+    timestamp: Date.now(),
+  },
 ];
 
 function App() {
-  const [feed] = useState<Feed[]>(DUMMY_FEED);
+  const [feed] = useState<Post[]>(DUMMY_FEED);
 
   return (
-    <div>
+    <div className="border-slate-200 border-t">
       {feed.map((f) => (
-        <div>
-          <div>{f.username}</div>
+        <div key={f.id} className="space-y-2 border-b border-slate-200 p-4">
+          <div className="flex space-x-2">
+            <h2 className="font-bold">@{f.username}</h2>
+            <span className="text-slate-600">
+              {new Date(f.timestamp).toLocaleString()}
+            </span>
+          </div>
           <div>{f.content}</div>
-          <div>{f.likes}</div>
-          <div>{f.retweets}</div>
-          <div>{f.comments}</div>
-          <div>{f.timestamp}</div>
+          <div className="flex space-x-2">
+            <div className="text-slate-600 flex space-x-1 items-center">
+              <BiComment />
+              <span>{f.likes}</span>
+            </div>
+            <div className="text-slate-600 flex space-x-2 items-center">
+              <BiComment />
+              <span>{f.retweets}</span>
+            </div>
+            <div className="text-slate-600 flex space-x-2 items-center">
+              <BiComment />
+              <span>{f.comments}</span>
+            </div>
+          </div>
         </div>
       ))}
     </div>
