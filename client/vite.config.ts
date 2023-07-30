9303f2f1ae502305747db81c5be5417b2dca5819
Kevin Hu <kevinhuwest@gmail.com>
2023-07-30 10:47:07 -0700

    Change favicon

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { ViteFaviconsPlugin } from "vite-plugin-favicon";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), ViteFaviconsPlugin("./src/components/twitter.svg")],
+});
